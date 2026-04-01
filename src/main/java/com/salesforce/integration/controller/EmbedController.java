6847e2657d294625f8713f0445528acad1b5ee61
521qinmi <521qinmi@163.com>
2026-04-01 18:57:49 +0800

    Update EmbedController.java

--- src/main/java/com/salesforce/integration/controller/EmbedController.java
+++ src/main/java/com/salesforce/integration/controller/EmbedController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.HashMap;
